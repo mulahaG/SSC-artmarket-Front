2d48a0cf74aea4a55677637cf18c7cb9ee7d2027
unknown <hanmajsh1@gmail.com>
2022-05-31 20:37:45 +0900

    오류해결

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+import './index.css'; 
+import App from './App'; 
+import reportWebVitals from './reportWebVitals';  
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(   <React.StrictMode>     <App />   </React.StrictMode> );  
+// If you want to start measuring performance in your app, pass a function 
+// to log results (for example: reportWebVitals(console.log)) 
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals 
 reportWebVitals();
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />,
-  </React.StrictMode>
-);
->>>>>>> bab6bbf4c3b75a5c7390a30d402f6e9f76ebad60
