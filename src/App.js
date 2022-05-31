7388f5144e8aee91771206954a93d4e8c9717927
unknown <hanmajsh1@gmail.com>
2022-05-31 20:24:09 +0900

    메뉴바

--- src/App.js
+++ src/App.js
@@ -6,9 +6,33 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <form>
+          <input type="search" className="App-Search"/>
+          <button className="search_button">
+            <img src="search.png" className="Search-logo" alt="Search" />
+            <span className="search_Text">검색</span>
+          </button>
+          <button className="like_button">
+            <img src="like.png" className="like-logo" alt="like" />
+            <span className="like_Text">관심등록</span>
+          </button>
+          <button className="login_button">
+            <img src="login.png" className="login-logo" alt="login" />
+            <span className="login_Text">로그인</span>
+          </button>
+          <button className="cart_button">
+            <img src="cart.png" className="cart-logo" alt="cart" />
+            <span className="cart_Text">장바구니</span>
+          </button>
+          <button className="customer_button">
+            <img src="customer.png" className="customer-logo" alt="customer" />
+            <span className="customer_Text">고객센터</span>
+          </button>
+        </form>
+
       </header>
       <bady className="App-bady">
-
+        
       </bady>
     </div>
   );
